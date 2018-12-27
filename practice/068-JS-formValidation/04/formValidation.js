23e2a12707a32f41ad54b9ee27c6aa3850efb511
bruceyu <bruceyuhongbo@gmail.com>
2018-12-28 06:16:24 +0800

    add JS formValidation #4

---
+++ practice/068-JS-formValidation/04/formValidation.js
@@ -0,0 +1,46 @@
+;
+(function () {
+  'use strict';
+
+  window.vali = {
+    between,
+    betweenLength,
+    isUsername,
+    isEmail,
+    isPhone,
+    isPassword,
+  }
+
+  function between(num, min, max) {
+    return num >= min && num <= max;
+  }
+
+  function betweenLength(str, min, max) {
+    return between(str.length, min, max);
+  }
+
+  function isUsername(str) {
+    if (!betweenLength(str, 4, 12) || str.includes('bitch'))
+      return false;
+    return true;
+  }
+
+  function isEmail(str) {
+    if (!str.includes('@'))
+      return false;
+    return true;
+  }
+
+  function isPhone(str) {
+    if ((str.length != 11 && str.length != 13 && str.length != 14) || !str.startsWith('1'))
+      return false;
+    return true;
+  }
+
+  function isPassword(str) {
+    if (!betweenLength(str, 6, 64))
+      return false;
+    return true;
+  }
+  
+})();

f1e62fefdf50448de91b2b975bb1715ec97ffffa
bruceyu <bruceyuhongbo@gmail.com>
2018-12-31 16:15:05 +0800

    add githubSearch #1

---
+++ practice/074-githubSearch/01/request.js
@@ -0,0 +1,28 @@
+;
+(function () {
+  'use strict';
+
+  window.re = {
+    get,
+    post,
+  };
+
+  function get(url, onSuccess, onError) {
+    send(url, 'get', null, onSuccess, onError);
+  }
+
+  function post(url, data) {
+    send(url, 'post', data, onSuccess, onError);
+  }
+
+  function send(url, type, data, onSuccess, onError) {
+    let http = new XMLHttpRequest();
+    http.open(type, url);
+    http.send();
+
+    http.addEventListener('load', () => {
+      let data = JSON.parse(http.responseText)
+      onSuccess && onSuccess(data);
+    })
+  }
+})()

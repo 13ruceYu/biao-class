fecd26d9551d8919a5d5e71d7aad66a4ea96cf5a
bruceyu <bruceyuhongbo@gmail.com>
2019-01-24 07:50:41 +0800

    add JS requestData #4

---
+++ practice/073(C61)-requestData/04/requestLib/rq.js
@@ -0,0 +1,30 @@
+;
+(function () {
+
+  'use strict';
+
+  window.rq = {
+    get,
+    post
+  };
+
+  function get(url, onSuccess, onError) {
+    send(url, 'get', null, onSuccess, onError);
+  }
+
+  function post(url, data, onSuccess, onError) {
+    send(url, 'post', data, onSuccess, onError)
+  }
+
+  function send(url, type, data, onSuccess, onError) {
+    let http = new XMLHttpRequest();
+    http.open(type, url);
+    http.send();
+
+    http.addEventListener('load', () => {
+      data = JSON.parse(http.responseText);
+      onSuccess && onSuccess(data);
+    })
+  }
+
+})();

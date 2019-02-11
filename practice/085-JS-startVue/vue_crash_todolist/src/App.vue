2ecb71bc7c8f0ca5576abe7f3bf4b2049f841756
bruceyu <bruceyuhongbo@gmail.com>
2019-02-11 23:20:36 +0800

    add vue start 20%

---
+++ practice/085-JS-startVue/vue_crash_todolist/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <Todos v-bind:todos="todos" />
+  </div>
+</template>
+
+<script>
+  import Todos from './components/Todos';
+
+  export default {
+    name: "app",
+    components: {
+      Todos,
+    },
+    data() {
+      return {
+        todos: [
+          {
+            id:1,
+            title: 'Todo One',
+            completed: false,
+          },
+          {
+            id:2,
+            title: 'Todo Two',
+            completed: true,
+          },
+          {
+            id:3,
+            title: 'Todo Three',
+            completed: false,
+          },
+        ]
+      }
+    },
+  };
+</script>
+
+<style>
+  * {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+  }
+
+  body {
+    font-family: Arial, Helvetica, sans-serif, 'Microsoft YaHei';
+  }
+
+
+</style>

a6f7aaebe6cdbe800dedac7d5094fea784ab096a
Bruce Yu <bruceyuhongbo@gmail.com>
2019-04-03 21:46:23 +0800

    start angular journey

---
+++ practice/091-angular-basic/angular-start/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AComponent } from './a/a.component';
+import { BComponent } from './b/b.component';
+import { CComponent } from './c/c.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AComponent,
+    BComponent,
+    CComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ practice/091-angular-basic/angular-start/src/app/c/c.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-c',
+  templateUrl: './c.component.html',
+  styles: []
+})
+export class CComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

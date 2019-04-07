b06500f4e26ecfd4a5f4d0ea931f8661bc488bb9
Bruce Yu <bruceyuhongbo@gmail.com>
2019-04-07 23:02:09 +0800

    add admin-user form table

--- project/007-vue-ssp/src/pages/admin/User.vue
+++ project/007-vue-ssp/src/pages/admin/User.vue
@@ -1,6 +1,46 @@
 <template>
   <div class="admin-user">
-    
+    <el-button>添加/更新用户</el-button>
+    <div class="form-user">
+      <form @submit="createOrUpdate">
+        <div class="input-field">
+          <div class="title">昵称</div>
+          <el-input></el-input>
+        </div>
+        <div class="input-field">
+          <div class="title">手机号</div>
+          <el-input></el-input>
+        </div>
+        <div class="input-field">
+          <div class="title">邮箱</div>
+          <el-input></el-input>
+        </div>
+        <div class="input-field">
+          <div class="title">密码</div>
+          <el-input></el-input>
+        </div>
+        <div class="el-input-field">
+          <el-button type="primary" native-type="submit">确定</el-button>
+          <el-button>取消</el-button>
+        </div>
+      </form>
+    </div>
+    <div class="table-user">
+      <el-table :data="tableData" style="width: 100%" max-height="250">
+        <el-table-column fixed prop="date" label="日期" width="150"></el-table-column>
+        <el-table-column prop="name" label="姓名" width="120"></el-table-column>
+        <el-table-column prop="province" label="省份" width="120"></el-table-column>
+        <el-table-column prop="city" label="市区" width="120"></el-table-column>
+        <el-table-column prop="address" label="地址" width="300"></el-table-column>
+        <el-table-column prop="zip" label="邮编" width="120"></el-table-column>
+        <el-table-column fixed="right" label="操作" width="160">
+          <template slot-scope="scope">
+            <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+            <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
   </div>
 </template>
 
@@ -8,11 +48,88 @@
 export default {
   data() {
     return {
-      
+      formUser: {},
+      tableData: [
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333
+        },
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333
+        },
+        {
+          date: "2016-05-08",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333
+        },
+        {
+          date: "2016-05-06",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333
+        },
+        {
+          date: "2016-05-07",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333
+        }
+      ]
     };
+  },
+  methods: {
+    createOrUpdate() {},
+    handleEdit() {},
+    handleDelete() {}
   }
 };
 </script>
 
 <style scoped>
+.form-user {
+  max-width: 500px;
+}
+
+.el-input-field {
+  margin: 15px 0;
+}
+
+.table-user {
+  border: 1px solid #ccc;
+  padding: 5px;
+  background: #fff;
+  box-shadow: 0 1px 2px rgba(46, 61, 73, 0.08);
+}
 </style>
